8934699c4679e221cee3a825a213e9013d8a2522
GreyKeenan <156843326+GreyKeenan@users.noreply.github.com>
2024-12-02 13:42:24 -0600

    splitting main()

--- main.c
+++ main.c
@@ -16,22 +16,22 @@
 
 #include "zoop/decompress.h"
 
+static inline int unwrap_png(const char *path, uint8_t **dataDest, uint32_t *lengthDest, struct Whine_Image *image);
+static inline int decompress(uint8_t *zlibData, uint32_t zlibDataLength, uint8_t **dataDest, uint32_t *lengthDest);
+
+
 int main(int argc, char **argv) {
 	Gunc_title("Starting Program: %s", argv[0]);
 
 	int e = 0;
 
-	struct Gunc_iByteStream bs = {0};
-	struct Gunc_iByteWriter bw = {0};
+	struct Whine_Image image;
 
-	FILE *f = NULL;
-	struct Gunc_ByteBalloon bb_whine = {0};
+	uint8_t *hnZlibData = NULL;
+	uint32_t zlibDataLength = 0;
 
-	struct Whine_Image image = {0};
-
-	struct Gunc_Sequence seq = {0};
-	struct Gunc_ByteBalloon bb_zoop = {0};
-	struct Gunc_iByteLooker bl = {0};
+	uint8_t *hnDecompressedData = NULL;
+	uint32_t decompressedDataLength = 0;
 
 	if (argc < 2) {
 		Gunc_err("missing png file path");
@@ -40,78 +40,146 @@ int main(int argc, char **argv) {
 	Gunc_say("filepath: %s", argv[1]);
 
 
+	e = unwrap_png(argv[1], &hnZlibData, &zlibDataLength, &image);
+	if (e) {
+		Gunc_nerr(e, "failed to unwrap png.");
+		goto fin;
+	}
+	Gunc_say("data: %p length: %d", hnZlibData, zlibDataLength);
+
+	e = decompress(hnZlibData, zlibDataLength, &hnDecompressedData, &decompressedDataLength);
+	if (e) {
+		Gunc_nerr(e, "failed to decompress zlib data.");
+		goto fin;
+	}
+
+	// free zlibData after using it
+	// set to null so no double free
+
+
+	fin:
+	if (hnZlibData != NULL) {
+		free(hnZlibData);
+	}
+	if (hnDecompressedData != NULL) {
+		free(hnDecompressedData);
+	}
+
+	Gunc_title("Program endpoint reached.");
+	return 0;
+}
+
+static inline int unwrap_png(const char *path, uint8_t **dataDest, uint32_t *lengthDest, struct Whine_Image *image) {
+
+	int e = 0;
+	int r = 0;
+
+	struct Gunc_iByteStream bs = {0};
+	struct Gunc_iByteWriter bw = {0};
+
+	FILE *f = NULL;
+	struct Gunc_ByteBalloon bb = {0};
+
+
 	Gunc_title("Whine_stripng Setup");
 
-	e = Gunc_file_as_iByteStream(&f, &bs, argv[1]);
+	e = Gunc_file_as_iByteStream(&f, &bs, path);
 	if (e) {
-		Gunc_nerr(e, "Failed to open file: %s", argv[1]);
+		Gunc_nerr(e, "Failed to open file: %s", path);
+		r = __LINE__;
 		goto fin;
 	}
 
-	e = Gunc_ByteBalloon_init(&bb_whine, 1024);
+	e = Gunc_ByteBalloon_init(&bb, 1024);
 	if (e) {
-		Gunc_nerr(e, "failed to init byteBalloon %p", &bb_whine);
+		Gunc_nerr(e, "failed to init byteBalloon %p", &bb);
+		r = __LINE__;
 		goto fin;
 	}
-	e = Gunc_ByteBalloon_as_iByteWriter(&bb_whine, &bw);
+	e = Gunc_ByteBalloon_as_iByteWriter(&bb, &bw);
 	if (e) {
 		Gunc_nerr(e, "failed to create iByteWriter from byteBalloon");
+		r = __LINE__;
 		goto fin;
 	}
 
 
 	Gunc_title("Stripping PNG");
 
-	e = Whine_stripng(&image, &bs, &bw);
+	e = Whine_stripng(image, &bs, &bw);
 	if (e) {
 		Gunc_nerr(e, "failed to extract zlib data from png");
+		r = __LINE__;
 		goto fin;
 	}
 
 
 	Gunc_title("Whine Closeout");
 
-	e = Gunc_ByteBalloon_trim(&bb_whine);
+	e = Gunc_ByteBalloon_trim(&bb);
 	if (e) {
 		Gunc_nerr(e, "failed to trim byteBalloon.");
+		r = __LINE__;
 		goto fin;
 	}
 
-	e = fclose(f);
-	if (e) {
-		Gunc_nwarn(e, "failed to close file (ptr: %p, path: %s). Continuing ... ", f, argv[1]);
-	} else {
-		f = NULL;
-	}
+	*dataDest = bb.hData;
+	bb.hData = NULL;
+	*lengthDest = bb.length;
 
+
+	fin:
+	if (bb.hData != NULL) {
+		free(bb.hData);
+	}
+	if (f != NULL) {
+		e = fclose(f);
+		if (e) {
+			Gunc_nerr(e, "failed to fclose() file");
+			r = __LINE__;
+		}
+	}
+	return r;
+}
+
+static inline int decompress(uint8_t *zlibData, uint32_t zlibDataLength, uint8_t **dataDest, uint32_t *lengthDest) {
+
+	int e = 0;
 
 	Gunc_title("Zoop setup");
 
-	e = Gunc_ByteBalloon_init(&bb_zoop, 1024);
+	struct Gunc_ByteBalloon bb = {0};
+	struct Gunc_iByteWriter bw = {0};
+	struct Gunc_iByteLooker bl = {0};
+
+	e = Gunc_ByteBalloon_init(&bb, 1024);
 	if (e) {
 		Gunc_nerr(e, "failed to init byteBalloon for zoop.");
-		goto fin;
+		return __LINE__;
 	}
-	e = Gunc_ByteBalloon_as_iByteWriter(&bb_zoop, &bw);
+	e = Gunc_ByteBalloon_as_iByteWriter(&bb, &bw);
 	if (e) {
 		Gunc_nerr(e, "failed to create iByteWriter from byteBalloon for zoop.");
-		goto fin;
+		return __LINE__;
 	}
-	e = Gunc_ByteBalloon_as_iByteLooker(&bb_zoop, &bl);
+	e = Gunc_ByteBalloon_as_iByteLooker(&bb, &bl);
 	if (e) {
 		Gunc_nerr(e, "failed to create iByteLooker from byteBalloon");
-		goto fin;
+		return __LINE__;
 	}
 
-	e = Gunc_Sequence_init(&seq, bb_whine.hData, bb_whine.length, sizeof(*bb_whine.hData));
+	struct Gunc_Sequence seq = {0};
+	struct Gunc_iByteStream bs = {0};
+
+	e = Gunc_Sequence_init(&seq, zlibData, zlibDataLength, sizeof(*zlibData));
 	if (e) {
 		Gunc_nerr(e, "failed to init Sequence from bb_whine");
-		goto fin;
+		return __LINE__;
 	}
 	e = Gunc_Sequence_as_iByteStream(&seq, &bs);
 	if (e) {
 		Gunc_nerr(e, "failed to create iByteStream from Sequence");
-		goto fin;
+		return __LINE__;
 	}
 
 	
@@ -120,30 +188,12 @@ int main(int argc, char **argv) {
 	e = Zoop_decompress(&bs, &bw, &bl);
 	if (e) {
 		Gunc_nerr(e, "failed to zoop the zoopidee zoop");
-		goto fin;
+		return __LINE__;
 	}
 
-
-	fin:
-
-	if (f != NULL) {
-		e = fclose(f);
-		if (e) {
-			Gunc_nwarn(e, "failed to close file (ptr: %p, path: %s). Program terminating soon anyways.", f, argv[1]);
-		}
-	}
-
-	if (bb_whine.hData != NULL) {
-		free(bb_whine.hData);
-	}
-	if (bb_zoop.hData != NULL) {
-		free(bb_zoop.hData);
-	}
-
-
-	Gunc_title("Program endpoint reached.");
 	return 0;
 }
+
 
 #include "gunc/log.c"
 #include "gunc/files.c"
