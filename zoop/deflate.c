2e9408ae214a9dce6502e9ed4f08c13d94e983fb
GreyKeenan <156843326+GreyKeenan@users.noreply.github.com>
2024-12-02 17:05:03 -0600

    deflate setup, handling block header

--- zoop/deflate.c
+++ zoop/deflate.c
@@ -7,5 +7,51 @@
 
 int Zoop_deflate(struct Gunc_BitStream *bis, struct Gunc_iByteWriter *bw, struct Gunc_iByteLooker *bl) {
 
+	int e = 0;
+	bool bit = 0;
 
+	bool isLastBlock = false;
+	uint8_t compressionType = 0;
+
+	do {
+		e = Gunc_BitStream_bit(bis, &isLastBlock);
+		if (e) {
+			Gunc_nerr(e, "failed to fetch bit");
+			return __LINE__;
+		}
+
+		e = Gunc_BitStream_bit(bis, &bit);
+		if (e) {
+			Gunc_nerr(e, "failed to fetch bit ct0");
+			return __LINE__;
+		}
+		compressionType = bit;
+		e = Gunc_BitStream_bit(bis, &bit);
+		if (e) {
+			Gunc_nerr(e, "failed to fetch bit ct1");
+			return __LINE__;
+		}
+		compressionType |= bit << 1;
+
+		switch (compressionType) {
+			case 0:
+				Gunc_TODO("uncompressed compression type");
+				break;
+			case 1:
+				Gunc_TODO("fixed compression type");
+				break;
+			case 2:
+				Gunc_TODO("dynamic compression type");
+				break;
+			case 3:
+				Gunc_err("reserved compression type (%d).", compressionType);
+				return __LINE__;
+			default:
+				Gunc_err("unrecognized compressionType. How did this even happen? (%d)", compressionType);
+				return __LINE__;
+		}
+
+	} while (!isLastBlock);
+
+	return 0;
 }
