dd6fd6677fe2344a2c594af3e6ac8fc77dc7b441
GreyKeenan <156843326+GreyKeenan@users.noreply.github.com>
2024-12-04 13:14:27 -0600

    distTree, litTree, metaTree structs & defines

---
+++ shrub/litTree.h
@@ -0,0 +1,19 @@
+#ifndef SHRUB_LitTree_H
+#define SHRUB_LitTree_H
+
+#include "./tree.h"
+
+#define Shrub_LitTree_MAXLEAVES 288
+#define Shrub_LitTree_BITSPERLEAF 9 //ceil(log2(MAXLEAVES))
+#define Shrub_LitTree_DATALENGTH Shrub_Tree_MEASURE(Shrub_LitTree_MAXLEAVES, Shrub_LitTree_BITSPERLEAF)
+
+struct Shrub_LitTree {
+	struct Shrub_Tree tree;
+	uint8_t data[Shrub_LitTree_DATALENGTH];
+};
+
+static inline int Shrub_LitTree_init(struct Shrub_LitTree *self) {
+	return Shrub_Tree_init(self->tree, self->data, Shrub_LitTree_DATALENGTH, Shrub_LitTree_MAXLEAVES, Shrub_LitTree_BITSPERLEAF);
+}
+
+#endif
---
+++ shrub/metaTree.h
@@ -0,0 +1,19 @@
+#ifndef SHRUB_MetaTree_H
+#define SHRUB_MetaTree_H
+
+#include "./tree.h"
+
+#define Shrub_MetaTree_MAXLEAVES 19
+#define Shrub_MetaTree_BITSPERLEAF 5 //ceil(log2(MAXLEAVES))
+#define Shrub_MetaTree_DATALENGTH Shrub_Tree_MEASURE(Shrub_MetaTree_MAXLEAVES, Shrub_MetaTree_BITSPERLEAF)
+
+struct Shrub_MetaTree {
+	struct Shrub_Tree tree;
+	uint8_t data[Shrub_MetaTree_DATALENGTH];
+};
+
+static inline int Shrub_MetaTree_init(struct Shrub_MetaTree *self) {
+	return Shrub_Tree_init(self->tree, self->data, Shrub_MetaTree_DATALENGTH, Shrub_MetaTree_MAXLEAVES, Shrub_MetaTree_BITSPERLEAF);
+}
+
+#endif
